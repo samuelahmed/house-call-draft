2ace5fd42060831b4c6033adce1a652b0a9e2adf
samuelahmed <samueladuval@gmail.com>
2022-12-13 11:58:34 -0800

    Comments regarding paths for dashboard page

--- src/pages/caregivers.tsx
+++ src/pages/caregivers.tsx
@@ -1,6 +1,12 @@
 import { type NextPage } from "next";
 
 
+
+//if not a member -> this links to a information / register page / login page
+//if member but wrong type (patient on caregiver page) -> this page will offer login as patient or redirect to caregiver
+//if member (cookie of acc logged in) -> this page is a dashboard
+
+
 const Caregivers: NextPage = () => {
     return (
         <div>
