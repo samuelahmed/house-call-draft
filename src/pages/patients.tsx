5ec06c90a1bb9eb178bc6d400928a71729a55ce3
samuelahmed <samueladuval@gmail.com>
2022-12-14 20:27:09 -0800

    Fix patient page, removing bloat

--- src/pages/patients.tsx
+++ src/pages/patients.tsx
@@ -1,27 +1,14 @@
 import { type NextPage } from "next";
-import Header from "./components/header"
-import { trpc } from '../utils/trpc';
+
 
 
 const Patients: NextPage = () => {
 
-const mutation = trpc.login.useMutation();
-const handleLogin = async () => {
-  const name = 'John Doe';
-  mutation.mutate({ name });
-};
+
 
     return (
       <div>
-        < Header />
-      <h1>Login Form
-
-        
-      </h1>
-      <button onClick={handleLogin} disabled={mutation.isLoading}>
-        Login
-      </button>
-      {mutation.error && <p>Something went wrong! {mutation.error.message}</p>}
+        Patients Dashboard
     </div>
     );
   }
