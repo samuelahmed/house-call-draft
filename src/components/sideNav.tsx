062276f19bf1f4514673124e9c2567a18f00fa72
samuelahmed <samueladuval@gmail.com>
2022-12-16 22:38:35 -0800

    Add sidebar and test page architecture

---
+++ src/components/sideNav.tsx
@@ -0,0 +1,45 @@
+import { Sidebar } from 'flowbite-react'
+import { HiChartPie,HiViewBoards,HiInbox,HiUser,HiShoppingBag,HiArrowSmRight,HiTable, } from "react-icons/hi";
+
+const SideNav = () => {
+
+  return (
+    <div className="w-fit">
+  <Sidebar aria-label="Sidebar with content separator example">
+    <Sidebar.Items>
+      <Sidebar.ItemGroup>
+        <Sidebar.Item
+          href="/dashboard"
+          icon={HiChartPie}
+        >
+          Dashboard
+        </Sidebar.Item>
+        <Sidebar.Item
+          href="/messages"
+          icon={HiInbox}
+        >
+          Messages
+        </Sidebar.Item>
+        <Sidebar.Item
+          href="/account"
+          icon={HiUser}
+        >
+          Account
+        </Sidebar.Item>
+      </Sidebar.ItemGroup>
+      <Sidebar.ItemGroup>
+        <Sidebar.Item
+          href="/help"
+          icon={HiViewBoards}
+        >
+          Help
+        </Sidebar.Item>
+      </Sidebar.ItemGroup>
+    </Sidebar.Items>
+  </Sidebar>
+</div>
+  );
+
+};
+
+export default SideNav;
