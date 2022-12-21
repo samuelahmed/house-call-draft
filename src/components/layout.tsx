769d381bd9752ac08a32897745ed06472d7acd63
samuelahmed <samueladuval@gmail.com>
2022-12-20 21:59:08 -0800

    Delete resize code - add again in future if necessary.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,29 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Header from "./layoutComponents/header";
 import SideNav from "./layoutComponents/sideNav";
 
 const Layout = () => {
-  const [isMobile, setIsMobile] = useState(false);
+
   const [showNav, setShowNav] = useState(false);
-
-  function handleResize() {
-    if (innerWidth <= 640) {
-      setIsMobile(true);
-      // setShowNav(false);
-    } else {
-      setIsMobile(false);
-      // setShowNav(true);
-    }
-  }
-
-  useEffect(() => {
-    if (typeof window !== undefined) {
-      addEventListener("resize", handleResize);
-    }
-    return () => {
-      removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
     <>
