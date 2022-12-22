456342298b4aecb92979304dda537fb438f3e10b
samuelahmed <samueladuval@gmail.com>
2022-12-21 20:35:57 -0800

    Build live for mobile testing

--- src/components/caregiverDashboardCustom.tsx
+++ src/components/caregiverDashboardCustom.tsx
@@ -1,4 +1,3 @@
-import TabsRender from "./dashboardBuildingBlocks/tabs";
 import { useState } from "react";
 
 const CaregiverDashboardCustom = ({ color }: { color: any }) => {
@@ -10,13 +9,15 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
         <h1>Caregiver Dashboard</h1>
       </div>
 
-
       {/* TABS */}
       <div className="grid w-11/12 grid-rows-1 items-stretch justify-items-center bg-gray-300">
         <div className="items grid w-full grid-cols-3 justify-items-start gap-0 text-center">
+
+
+
           <a
             className={
-              "h-16 w-full border-2 border-t-black border-l-black border-b-black" +
+              "h-16 w-full " +
               (openTab === 1
                 ? "bg- text-white" + color + "-600"
                 : "text-" + color + "-600 bg-white")
@@ -29,12 +30,13 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
             href="#link1"
             role="tablist"
           >
-            tab 1
+            <div className="pb-4 pt-4 text-md md:text-xl">
+              <h1>Find Patient</h1>
+            </div>
           </a>
-
           <a
             className={
-              "h-16 w-full border-2 border-t-black border-l-black border-b-black" +
+              "h-16 w-full " +
               (openTab === 2
                 ? "bg- text-white" + color + "-600"
                 : "text-" + color + "-600 bg-white")
@@ -47,12 +49,17 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
             href="#link2"
             role="tablist"
           >
-            tab 2
+            <div className="pb-4 pt-4 text-md md:text-xl">
+              <h1>Scheduled Sessions</h1>
+            </div>
           </a>
+
+
+
 
           <a
             className={
-              "h-16 w-full border-2 border-t-black border-l-black border-b-black" +
+              "h-16 w-full " +
               (openTab === 3
                 ? "bg- text-white" + color + "-600"
                 : "text-" + color + "-600 bg-white")
@@ -65,78 +72,80 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
             href="#link3"
             role="tablist"
           >
-            tab 3
+            <div className="pb-4 pt-4 text-md md:text-xl">
+              <h1>History</h1>
+            </div>
           </a>
+
+
         </div>
       </div>
 
+      <div
+        className={
+          openTab === 1 ? "block grid w-full justify-items-center" : "hidden"
+        }
+        id="link1"
+      >
+        {/* SEARCH */}
+        <div className="grid w-11/12 grid-rows-1 items-stretch justify-items-center bg-gray-300 ">
+          <div className="h-14 w-full text-center">Search</div>
+        </div>
 
+        {/* MAIN SECTION */}
+        <div className="grid w-11/12 grid-rows-1 items-stretch  bg-gray-300 px-4 py-4">
+          <div className="grid h-128 grid-cols-1 md:grid-cols-2">
+            {/* Job Session Card */}
+            <div className="h-128 overflow-scroll pr-2">
+              <div className="grid grid-rows-3 justify-items-center gap-6 bg-red-200 pt-4 pb-6">
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
+                <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
 
-      <div className={openTab === 1 ? "block" : "hidden"} id="link1">
+  
 
-         
-      {/* SEARCH */}
-      <div className="grid w-11/12 grid-rows-1 items-stretch justify-items-center bg-gray-300 ">
-        <div className="h-14 w-full text-center">Search</div>
-      </div>
-
-      {/* MAIN SECTION */}
-      <div className="grid w-11/12 grid-rows-1 items-stretch  bg-gray-300 px-4 py-4">
-        <div className="grid h-128 grid-cols-1 md:grid-cols-2">
-          {/* Job Session Card */}
-          <div className="h-128 overflow-scroll pr-2">
-            <div className="grid grid-rows-3 justify-items-center gap-6 bg-red-200 pt-4 pb-6">
-              <div className="h-44 w-9/12 bg-yellow-200"> card 1</div>
-              <div className="w-9/12 bg-yellow-200"> card 1</div>
-              <div className="w-9/12 bg-yellow-200"> card 1</div>
+              </div>
             </div>
-          </div>
 
-          {/* Job Session Details Card */}
-          <div className="h-128 overflow-scroll pl-2">
-            <div className="grid h-128 grid-rows-1 justify-items-center gap-6 bg-red-200 pt-4 pb-6 ">
-              <div className="w-9/12 bg-yellow-200"> card 1</div>
+            {/* Job Session Details Card */}
+            <div className="h-128 overflow-scroll pl-2">
+              <div className="grid h-128 grid-rows-1 justify-items-center gap-6 bg-red-200 pt-4 pb-6 ">
+                <div className="w-9/12 bg-yellow-200"> card 1</div>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-
-
-
-</div>
-
-
-
-<div className={openTab === 2 ? "block" : "hidden"} id="link2">
-<p>
-  Completely synergize resource taxing relationships via
-  premier niche markets. Professionally cultivate one-to-one
-  customer service with robust ideas.
-  <br />
-  <br />
-  Dynamically innovate resource-leveling customer service for
-  state of the art customer service.
-</p>
-</div>
-<div className={openTab === 3 ? "block" : "hidden"} id="link3">
-<p>
-  Efficiently unleash cross-media information without
-  cross-media value. Quickly maximize timely deliverables for
-  real-time schemas.
-  <br />
-  <br /> Dramatically maintain clicks-and-mortar solutions
-  without functional solutions.
-</p>
-</div>
-
-
-
-
-
-
-
-
+      <div className={openTab === 2 ? "block" : "hidden"} id="link2">
+        <p>
+          Completely synergize resource taxing relationships via premier niche
+          markets. Professionally cultivate one-to-one customer service with
+          robust ideas.
+          <br />
+          <br />
+          Dynamically innovate resource-leveling customer service for state of
+          the art customer service.
+        </p>
+      </div>
+      <div className={openTab === 3 ? "block" : "hidden"} id="link3">
+        <p>
+          Efficiently unleash cross-media information without cross-media value.
+          Quickly maximize timely deliverables for real-time schemas.
+          <br />
+          <br /> Dramatically maintain clicks-and-mortar solutions without
+          functional solutions.
+        </p>
+      </div>
     </>
   );
 };
