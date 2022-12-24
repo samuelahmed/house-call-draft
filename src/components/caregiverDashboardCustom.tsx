113f871723d78dac825bff449bdb83b1f14ec7f3
samuelahmed <samueladuval@gmail.com>
2022-12-23 16:29:00 -0800

    Add modal feature for mobile. Push live for testing.

--- src/components/caregiverDashboardCustom.tsx
+++ src/components/caregiverDashboardCustom.tsx
@@ -1,14 +1,112 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const CaregiverDashboardCustom = ({ color }: { color: any }) => {
   const [openTab, setOpenTab] = useState(1);
   const [rightCard, setRightCard] = useState(1);
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
       <div className="pb-4 pt-4 text-3xl">
         <h1>Caregiver Dashboard</h1>
       </div>
+
+      {/* MODAL */}
+      {showModal ? (
+        <>
+          <div className="fixed z-50 w-11/12 justify-items-center rounded border-2 border-gray-300 outline-none focus:outline-none">
+            <div className=" relative mx-auto ">
+              <div className=" relative flex flex-col bg-white  shadow-lg outline-none focus:outline-none">
+                <div className="h-128 rounded-t border-gray-300 ">
+                  <div className="w- mt-4 flex items-center justify-around text-sm">
+                    <p className="text-gray-900">Milpitas</p>
+                    <p className="text-gray-900">Saturday January 3, 2023</p>
+                    <p className="text-gray-900">6:00am - 11:00am</p>
+                    <p className="text-gray-900">$35 / hour</p>
+                  </div>
+                  <div className="mb-4 mr-4 ml-4">
+                    <div className="mb-2 p-4 text-center text-xl  text-gray-800">
+                      Mobility Support
+                    </div>
+
+                    <div className="text-sm">
+                      <p className="text-gray-900">
+                        <span className="font-semibold text-gray-900">
+                          Pay per Hour:&nbsp;
+                        </span>
+                        $35
+                      </p>
+                      <p className="text-gray-900">
+                        <span className="font-semibold text-gray-900">
+                          Total Compensation:&nbsp;
+                        </span>
+                        $175
+                      </p>
+                      <p className="text-gray-900">
+                        <span className="font-semibold text-gray-900">
+                          Date:&nbsp;
+                        </span>
+                        Friday January 3, 2023
+                      </p>
+                      <p className="text-gray-900">
+                        <span className="font-semibold text-gray-900">
+                          Time:&nbsp;
+                        </span>
+                        6:00am - 11:00am
+                      </p>
+                      <p className="text-gray-900">
+                        <span className="font-semibold text-gray-900">
+                          Total Hours:&nbsp;
+                        </span>
+                        5
+                      </p>
+                      <p className="text-gray-900">
+                        <span className="font-semibold text-gray-900">
+                          Address:&nbsp;
+                        </span>
+                        343 Bay Drive, Milpitas
+                      </p>
+                      <p className="text-gray-900">
+                        <span className="font-semibold text-gray-900">
+                          Age:&nbsp;
+                        </span>
+                        66
+                      </p>
+                      <p className="text-gray-900">
+                        <span className="font-semibold text-gray-900">
+                          Medical Notes:&nbsp;
+                        </span>
+                        Broke Wrist earlier this year
+                      </p>
+                    </div>
+                    <p className="pt-4 text-base text-gray-800">
+                      <span className="font-semibold text-gray-900">
+                        Overview:&nbsp;
+                      </span>
+                      I need help walking around and taking walks to the park to
+                      stay active
+                    </p>
+                  </div>
+                  <div className="mb-4 mt-4 flex justify-around">
+                    <button
+                      onClick={() => setShowModal(false)}
+                      className="h-10 rounded border border-gray-500 bg-transparent px-4 pt-2 pb-8 font-semibold text-gray-700 hover:border-gray-700 hover:bg-emerald-200 hover:text-black"
+                    >
+                      Schedule Session
+                    </button>
+                    <button
+                      onClick={() => setShowModal(false)}
+                      className="h-10 rounded border border-gray-500 bg-transparent px-4 pt-2 pb-8 font-semibold text-gray-700 hover:border-gray-700 hover:bg-red-200 hover:text-black"
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
 
       {/* TABS */}
       <div className="grid w-11/12 grid-rows-1 items-stretch justify-items-center rounded bg-gray-100">
@@ -138,7 +236,7 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
         <div className=" grid w-full grid-rows-1 items-stretch rounded-b  bg-gray-100 px-4">
           <div className="grid grid-cols-1 pt-2 pb-2 md:grid-cols-2">
             {/* Job Session Card */}
-            <div className="h-80 overflow-scroll pr-2 md:h-128">
+            <div className="h-128 overflow-scroll pr-2 md:h-128 lg:h-128">
               <div className="grid justify-items-center gap-4 rounded bg-gray-200 pt-6 pb-6">
                 <div
                   className="flex w-11/12 cursor-pointer flex-col  justify-between  rounded-xl border border-gray-400 bg-white p-2 leading-normal hover:bg-gray-100"
@@ -164,6 +262,14 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
                       now.
                     </p>
                   </div>
+                  <div className="mb-4 mt-4 flex justify-around  md:hidden lg:hidden">
+                    <button
+                      className="h-10 w-1/2 rounded border border-gray-500 bg-transparent px-4 pt-2 pb-8 font-semibold text-gray-700 hover:border-gray-700 hover:bg-emerald-200 hover:text-black"
+                      onClick={() => setShowModal(true)}
+                    >
+                      Learn More
+                    </button>
+                  </div>
                 </div>
 
                 <div
@@ -184,8 +290,17 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
                       Mobility Support
                     </div>
                     <p className="text-base text-gray-700">
-                      Overview: I need help walking around and taking walks to the park to stay active
+                      Overview: I need help walking around and taking walks to
+                      the park to stay active
                     </p>
+                  </div>
+                  <div className="mb-4 mt-4 flex justify-around  md:hidden lg:hidden">
+                    <button
+                      className="h-10 w-1/2 rounded border border-gray-500 bg-transparent px-4 pt-2 pb-8 font-semibold text-gray-700 hover:border-gray-700 hover:bg-emerald-200 hover:text-black"
+                      onClick={() => setShowModal(true)}
+                    >
+                      Learn More
+                    </button>
                   </div>
                 </div>
 
@@ -207,9 +322,17 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
                       Personal Care
                     </div>
                     <p className="text-base text-gray-700">
-                      Overview: Need support throughout the day with general activities
-
+                      Overview: Need support throughout the day with general
+                      activities
                     </p>
+                  </div>
+                  <div className="mb-4 mt-4 flex justify-around  md:hidden lg:hidden">
+                    <button
+                      className="h-10 w-1/2 rounded border border-gray-500 bg-transparent px-4 pt-2 pb-8 font-semibold text-gray-700 hover:border-gray-700 hover:bg-emerald-200 hover:text-black"
+                      onClick={() => setShowModal(true)}
+                    >
+                      Learn More
+                    </button>
                   </div>
                 </div>
 
@@ -237,6 +360,14 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
                       now.
                     </p>
                   </div>
+                  <div className="mb-4 mt-4 flex justify-around  md:hidden lg:hidden">
+                    <button
+                      className="h-10 w-1/2 rounded border border-gray-500 bg-transparent px-4 pt-2 pb-8 font-semibold text-gray-700 hover:border-gray-700 hover:bg-emerald-200 hover:text-black"
+                      onClick={() => setShowModal(true)}
+                    >
+                      Learn More
+                    </button>
+                  </div>
                 </div>
 
                 <div
@@ -263,20 +394,24 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
                       now.
                     </p>
                   </div>
+                  <div className="mb-4 mt-4 flex justify-around  md:hidden lg:hidden">
+                    <button
+                      className="h-10 w-1/2 rounded border border-gray-500 bg-transparent px-4 pt-2 pb-8 font-semibold text-gray-700 hover:border-gray-700 hover:bg-emerald-200 hover:text-black"
+                      onClick={() => setShowModal(true)}
+                    >
+                      Learn More
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
 
             {/* Job Session Details Card */}
 
-            <div className="h-80 overflow-scroll pr-2 md:h-128">
+            <div className="hidden h-80 overflow-scroll pr-2 md:block md:h-128 lg:block">
               <div className="grid h-128 justify-items-center   gap-4 rounded bg-gray-200 pt-6 pb-6">
                 <div className="h-11/12 flex w-11/12  flex-col  justify-between rounded-xl border border-gray-400 bg-white p-2 leading-normal">
                   <div className={rightCard === 1 ? "" : "hidden"}>
-
-
-
-
                     <div className="mt-4 flex items-center justify-around text-sm">
                       <p className="text-gray-900">San Jose</p>
                       <p className="text-gray-900">Saturday January 7, 2023</p>
@@ -327,7 +462,7 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
                         </p>
                         <p className="text-gray-900">
                           <span className="font-semibold text-gray-900">
-                            Age:&nbsp; 
+                            Age:&nbsp;
                           </span>
                           76
                         </p>
@@ -355,20 +490,10 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
                         Report Post
                       </button>
                     </div>
-
-
-
-
                   </div>
 
-
-
-
-
                   <div className={rightCard === 2 ? "" : "hidden"}>
-
-
-                  <div className="mt-4 flex items-center justify-around text-sm">
+                    <div className="mt-4 flex items-center justify-around text-sm">
                       <p className="text-gray-900">Milpitas</p>
                       <p className="text-gray-900">Saturday January 3, 2023</p>
                       <p className="text-gray-900">6:00am - 11:00am</p>
@@ -412,19 +537,19 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
                         </p>
                         <p className="text-gray-900">
                           <span className="font-semibold text-gray-900">
-                            Address:&nbsp; 
+                            Address:&nbsp;
                           </span>
-                               343 Bay Drive, Milpitas
+                          343 Bay Drive, Milpitas
                         </p>
                         <p className="text-gray-900">
                           <span className="font-semibold text-gray-900">
-                            Age:&nbsp; 
+                            Age:&nbsp;
                           </span>
                           66
                         </p>
                         <p className="text-gray-900">
                           <span className="font-semibold text-gray-900">
-                            Medical Notes:&nbsp; 
+                            Medical Notes:&nbsp;
                           </span>
                           Broke Wrist earlier this year
                         </p>
@@ -433,7 +558,8 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
                         <span className="font-semibold text-gray-900">
                           Overview:&nbsp;
                         </span>
-                        I need help walking around and taking walks to the park to stay active
+                        I need help walking around and taking walks to the park
+                        to stay active
                       </p>
                     </div>
                     <div className="mb-4 mt-4 flex justify-around">
@@ -446,11 +572,8 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
                     </div>
                   </div>
 
-
-
                   <div className={rightCard === 3 ? "" : "hidden"}>
-
-                  <div className="mt-4 flex items-center justify-around text-sm">
+                    <div className="mt-4 flex items-center justify-around text-sm">
                       <p className="text-gray-900">Mountain View</p>
                       <p className="text-gray-900">Saturday January 4, 2023</p>
                       <p className="text-gray-900">9:00am - 5:00pm</p>
@@ -494,19 +617,19 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
                         </p>
                         <p className="text-gray-900">
                           <span className="font-semibold text-gray-900">
-                            Address:&nbsp; 
+                            Address:&nbsp;
                           </span>
-                               113 Bloom Street, Mountain View
+                          113 Bloom Street, Mountain View
                         </p>
                         <p className="text-gray-900">
                           <span className="font-semibold text-gray-900">
-                            Age:&nbsp; 
+                            Age:&nbsp;
                           </span>
                           72
                         </p>
                         <p className="text-gray-900">
                           <span className="font-semibold text-gray-900">
-                            Medical Notes:&nbsp; 
+                            Medical Notes:&nbsp;
                           </span>
                           Diabetic
                         </p>
