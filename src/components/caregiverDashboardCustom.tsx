64a6b68d35965d8ee3cc2112812c06ef3f39f332
samuelahmed <samueladuval@gmail.com>
2022-12-23 20:36:28 -0800

    fix: removed test padding

--- src/components/caregiverDashboardCustom.tsx
+++ src/components/caregiverDashboardCustom.tsx
@@ -7,7 +7,7 @@ const CaregiverDashboardCustom = ({ color }: { color: any }) => {
 
   return (
     <>
-      <div className="pb-4 pt-4 text-3xl dark:pb-11 dark:text-white">
+      <div className="pb-4 pt-4 text-3xl dark:text-white">
         <h1>Caregiver Dashboard</h1>
       </div>
 
