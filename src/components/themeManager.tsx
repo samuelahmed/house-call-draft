7db3ad63ce93dda4910ad44f443dd107c14b7571
samuelahmed <samueladuval@gmail.com>
2022-12-23 19:20:24 -0800

    Note: reivew themeManager for possible hydration mismatch error

--- src/components/themeManager.tsx
+++ src/components/themeManager.tsx
@@ -1,15 +1,21 @@
 import { useTheme } from 'next-themes'
 
 // Do NOT use this! It will throw a hydration mismatch error.
+//Fix hydration error? 
+
+
+
 const ThemeManager = () => {
   const { theme, setTheme } = useTheme()
-
   return (
-    <select value={theme} onChange={e => setTheme(e.target.value)}>
-      {/* <option value="system">System</option> */}
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
-    </select>
+    <button
+    className="py-1 px-2 text-white dark:text-black bg-black dark:bg-white font-semibold rounded-md"
+    onClick={() => {
+      setTheme(theme === 'light' ? 'dark' : 'light')
+    }}
+  >
+    Lights
+  </button>
   )
 }
 
