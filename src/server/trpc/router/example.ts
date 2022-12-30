6e209f830f672fd12fc3dc5c471dc3c05a8249d1
samuelahmed <samueladuval@gmail.com>
2022-12-29 20:14:24 -0800

    removed relative url and disabled filter

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -4,9 +4,9 @@ export const exampleRouter = router({
 
   getOne: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.user.findFirst({
-      where: {
-        id: "clbmqwysx0000uqn19zbyi9h1",
-      },
+      // where: {
+      //   id: "clbmqwysx0000uqn19zbyi9h1",
+      // },
     });
   }),
 
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -6,7 +6,7 @@ import superjson from "superjson";
 import { type AppRouter } from "../server/trpc/router/_app";
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return "https://house-call-draft.vercel.app/messages"; // browser should use relative url
+  if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
