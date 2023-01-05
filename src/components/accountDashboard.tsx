377ee8f78b9f35d90164a42ca9a64aa0fe1559bc
samuelahmed <samueladuval@gmail.com>
2023-01-05 15:43:28 -0800

    Deleted unused components

--- src/components/accountDashboard.tsx
+++
@@ -1,9 +0,0 @@
-import AccountInformation from "./accountDashboardComponents/accountInformation";
-
-const AccountDashboard = () => {
-  //TEMPORARY ACCOUNT PAGE - UPDATE STYLING
-
-  return <AccountInformation />;
-};
-
-export default AccountDashboard;
