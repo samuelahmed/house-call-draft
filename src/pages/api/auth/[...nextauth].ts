0fa053bdba26dd9b78151f00cd9b384126734966
samuelahmed <samueladuval@gmail.com>
2023-01-11 16:02:41 -0800

    Test: credentials basic template

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -51,42 +51,68 @@ export const authOptions: NextAuthOptions = {
   session: { strategy: "jwt" },
   providers: [
     Credentials({
-      name: "credentials",
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "jsmith@gmail.com",
-        },
-        password: { label: "Password", type: "password" },
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
       },
-      authorize: async (credentials) => {
-        const cred = await loginSchema.parseAsync(credentials);
-
-        const user = await prisma.user.findFirst({
-          where: { email: cred.email },
-        });
-
-        if (!user) {
-          return null;
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+  
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+  
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
-
-        const isValidPassword = bcrypt.compareSync(
-          cred.password,
-          user.password
-        );
-
-        if (!isValidPassword) {
-          return null;
-        }
-
-        return {
-          id: user.id,
-          email: user.email,
-          username: user.username,
-        }
-      },
+      }
     }),
+    // Credentials({
+    //   name: "credentials",
+    //   credentials: {
+    //     email: {
+    //       label: "Email",
+    //       type: "email",
+    //       placeholder: "jsmith@gmail.com",
+    //     },
+    //     password: { label: "Password", type: "password" },
+    //   },
+    //   authorize: async (credentials) => {
+    //     const cred = await loginSchema.parseAsync(credentials);
+
+    //     const user = await prisma.user.findFirst({
+    //       where: { email: cred.email },
+    //     });
+
+    //     if (!user) {
+    //       return null;
+    //     }
+
+    //     const isValidPassword = bcrypt.compareSync(
+    //       cred.password,
+    //       user.password
+    //     );
+
+    //     if (!isValidPassword) {
+    //       return null;
+    //     }
+
+    //     return {
+    //       id: user.id,
+    //       email: user.email,
+    //       username: user.username,
+    //     }
+    //   },
+    // }),
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
