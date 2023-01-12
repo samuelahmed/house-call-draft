94d9111740941c7b70ecbc1c836701985571d386
samuelahmed <samueladuval@gmail.com>
2023-01-11 16:50:54 -0800

    Fix: authOptions

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,24 +1,42 @@
-// import NextAuth, { type NextAuthOptions } from "next-auth";
+import NextAuth, { type NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials"
-import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+// import NextAuth from "next-auth";
 
-
-export default NextAuth({
-  secret: env.NEXTAUTH_SECRET,
+// export default NextAuth({
+  export const authOptions: NextAuthOptions = {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   adapter: PrismaAdapter(prisma),
-  session: { strategy: "jwt" },
+  session: {
+    strategy: "jwt",
+  },
+  secret: env.NEXTAUTH_SECRET,
+  // pages: {
+  //   signIn: "/auth/login",
+  //   signOut: "/auth/logout",
+  //   error: "/auth/error",
+  //   verifyRequest: "/auth/verify",
+  // },
   providers: [
     CredentialsProvider({
       id: "credentials",
       name: "HouseCall",
       type: "credentials",
       credentials: {
-        email: { label: "Email Address", type: "email", placeholder: "meowcat@gmail.com" },
-        password: { label: "Password", type: "password", placeholder: "secureKitty.3x14" },
+        email: {
+          label: "Email Address",
+          type: "email",
+          placeholder: "meowcat@gmail.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "secureKitty.3x14",
+        },
       },
       async authorize(credentials) {
         if (!credentials) {
@@ -39,10 +57,10 @@ export default NextAuth({
           },
         });
         if (!user) {
-          throw new Error('Meow, no user');
+          throw new Error("Meow, no user");
         }
         if (!user.password) {
-          throw new Error('Meow, no password');
+          throw new Error("Meow, no password");
         }
         return {
           id: user.id,
@@ -58,5 +76,6 @@ export default NextAuth({
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-});
+};
 
+export default NextAuth(authOptions);
